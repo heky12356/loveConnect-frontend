c34749bb3f28118d3b12477771f56c9d37c4523d
heky12356 <heji12356@gmail.com>
2025-08-21 13:01:24 +0800

    feat(navbar): 为返回按钮添加路由返回功能
    添加点击返回按钮时的路由返回功能，提升用户导航体验

--- components/normalNavBar.tsx
+++ components/normalNavBar.tsx
@@ -1,12 +1,15 @@
 import AntDesign from '@expo/vector-icons/AntDesign';
 import Entypo from '@expo/vector-icons/Entypo';
+import { router } from 'expo-router';
 import { Dimensions, StyleSheet, View } from "react-native";
 const { height, width } = Dimensions.get("window");
 
 export default function NormalNavBar() {
     return (
         <View style={style.navbar}>
-            <AntDesign name="left" size={24} color="black" />
+            <AntDesign name="left" size={24} color="black" onPress={()=>{
+                router.back()
+            }} />
             <Entypo name="menu" size={28} color="black" />
         </View>
     )
